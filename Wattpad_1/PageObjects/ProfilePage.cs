fa4dfd5955ca91227bd5d3ef4622c7d076804022
JeanRibac <jeanribac95@gmail.com>
2020-04-06 01:23:02 +0400

    TM2

--- Wattpad_1/PageObjects/ProfilePage.cs
+++ Wattpad_1/PageObjects/ProfilePage.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
 namespace Wattpad_1.PageObjects
 {
@@ -29,9 +30,9 @@ namespace Wattpad_1.PageObjects
             BtnGotIt().Click();
 
         }
-        private By EditProfileButton => By.XPath("//*[@id='page-navigation']/div[2]/div/div/button");
-        //*[@id='page-navigation']/div[2]/div/div/button
 
+        //
+        private By EditProfileButton => By.XPath("//*[@id='page-navigation']/div[2]/div/div/button");
         private IWebElement BtnEditButton()
         {
             return driver.FindElement(EditProfileButton);
@@ -44,12 +45,13 @@ namespace Wattpad_1.PageObjects
             BtnEditButton().Click();
 
         }
-
+        //
         private By EditLocationInput => By.Id("update-location");
         private IWebElement InputLocation()
         {
             return driver.FindElement(EditLocationInput);
         }
+        //
         private By SaveChangesButton => By.CssSelector("button[class='btn btn-orange on-edit-save']");
         private IWebElement ClickSaveChangesButton()
         {
@@ -63,12 +65,103 @@ namespace Wattpad_1.PageObjects
             InputLocation().SendKeys(location);
             ClickSaveChangesButton().Click();
         }
-
+        //
         private By ProfileLocation => By.XPath("//*[@id='profile-about']/div/div/ul/li[1]/span");
         private IWebElement LblProfileLocation()
         {
             return driver.FindElement(ProfileLocation);
         }
         public string UserProfileLocation => LblProfileLocation().Text;
+        //
+
+        private By Conversations => By.XPath("//*[@id='page-navigation']/div[2]/div/nav/ul/li[2]/a");
+
+        private IWebElement btnConversations()
+        {
+            return driver.FindElement(Conversations);
+        }
+        public void ClickConversationsButton()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(driver => driver.FindElement(EditProfileButton));
+            btnConversations().Click();
+        }
+        //
+        private By postMessage => By.XPath("//*[@id='broadcast-container']/div[2]/textarea");
+        private IWebElement inputPostMessage()
+        {
+            return driver.FindElement(postMessage);
+        }
+        private By postMessageActive => By.CssSelector("textarea[class='form-control count-input new-textarea active']");
+        private IWebElement inputPostMessageActive()
+        {
+            return driver.FindElement(postMessageActive);
+        }
+        private By AnnounceToFollowers => By.XPath("//*[@id='broadcast-container']/div[2]/div[2]/div/input");
+        private IWebElement AnnounceCheckbox()
+        {
+            return driver.FindElement(AnnounceToFollowers);
+        }
+        private By GotIt => By.XPath("//*[@id='component-announcementmodal-announcement-modal-/user/stefana_qa/conversations']/div/div[2]/button");
+        private IWebElement GotItButton()
+        {
+            return driver.FindElement(GotIt);
+        }
+        private By Announce => By.XPath("//*[@id='broadcast-submit']");
+        private IWebElement AnnounceButton()
+        {
+            return driver.FindElement(Announce);
+        }
+        public void ClickWritePostMessage()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(driver => driver.FindElement(postMessage));
+            inputPostMessage().Click();
+            System.Threading.Thread.Sleep(100);
+        }
+        public void WritePostMessage(string post)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementExists(postMessageActive));
+            inputPostMessageActive().SendKeys(post);
+            System.Threading.Thread.Sleep(100);
+        }
+        public void ClickPostCheckbox()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(driver => driver.FindElement(postMessageActive));
+            AnnounceCheckbox().Click();
+            System.Threading.Thread.Sleep(150);
+
+        }
+        public void ClickGotItButton()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(driver => driver.FindElement(GotIt));
+            GotItButton().Click();
+        }
+        
+        public void ClickAnnounceButton()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+            wait.Until(driver => driver.FindElement(GotIt));
+            AnnounceButton().Click();
+            System.Threading.Thread.Sleep(500);
+
+        }
+
+        private By PostTime => By.CssSelector("time[class='timestamp']");
+        private IWebElement PostTimer()
+        {
+            return driver.FindElement(PostTime);
+        }
+        public string PostAddedTime => PostTimer().Text;
+        //
+        private By Pinned => By.XPath("//*[@id='profile-messages']/div/article");
+        private IWebElement PinnedPost()
+        {
+            return driver.FindElement(Pinned);
+        }
+        public string PostPinned => PinnedPost().Text;
     }
 }
---
+++ Wattpad_1/PostNewConversation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Wattpad_1.PageObjects;
+
+namespace Wattpad_1
+{
+    [TestClass]
+    public class PostNewConversation
+    {
+        private IWebDriver driver;
+        private LoginPage loginPage;
+        private ProfilePage profilePage;
+        private HomePage homePage;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            loginPage = new LoginPage(driver);
+            homePage = new HomePage(driver);
+            profilePage = new ProfilePage(driver);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.wattpad.com/");
+            loginPage.NavigateToLoginPage();
+        }
+        [TestMethod]
+        public void Post_New_Conversation()
+        {
+            loginPage.LoginApplication("stefanatoma98@gmail.com", "testareautomata");
+            homePage.NavigateToProfileDropdown();
+            homePage.NavigateToProfilePage();
+            profilePage.ClickKeepTrackButton();
+            profilePage.ClickConversationsButton();
+            profilePage.ClickWritePostMessage();
+            profilePage.WritePostMessage("Automation testing is the best");
+            profilePage.ClickPostCheckbox();
+            profilePage.ClickGotItButton();
+            profilePage.ClickAnnounceButton();
+            
+            var expectedResult = "a few seconds ago";
+            //var expectedResult2 = profilePage.PostPinned;
+
+            Assert.AreEqual(expectedResult, profilePage.PostAddedTime);
+            //Assert.AreEqual(expectedResult2, profilePage.PostPinned);
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
